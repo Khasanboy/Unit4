b77d82175db1a9efa8e7a9982299da52c8f67536
KhasanboyAkbarov <khasanboy.akbarov@ganalogics.com>
2017-10-30 12:35:56 +0200

    table is almost ready

--- demoApp/src/app/components/task4/task4.component.ts
+++ demoApp/src/app/components/task4/task4.component.ts
@@ -20,7 +20,7 @@ export class Task4Component implements OnInit {
 
   ngOnInit() {
     
-    this.currencies = [new Currency(1, "Eur", 1, true), new Currency(2, "Usd", 0.84, false)];
+    this.currencies = [new Currency(1, "EUR", 1, true), new Currency(2, "USD", 1.16, false),  new Currency(3, "SEK", 1.6, false),  new Currency(4, "GBP", 1.13, false)];
     let period1 = new Period(1, '2005.01', this.currencies);
     let period2 = new Period(2, '2005.02', this.currencies);
 
@@ -31,6 +31,14 @@ export class Task4Component implements OnInit {
   setCurrentPeriod(period){
     this.currentPeriod = period;
     console.log(this.currentPeriod);
+  }
+
+  getBaseCurrency(){
+    return this.currencies[1];
+  }
+
+  calculateRate(cur1, cur2){
+    return cur1.rate/cur2.rate;
   }
 
 }
